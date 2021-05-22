425faef9bbeb57a65ac7d707957611e6323e8b2f
vivekghevariya <vivekghevariya83@gmal.com>
2021-05-22 19:06:51 -0400

    New updated

--- src/ArrayPractice.java
+++ src/ArrayPractice.java
@@ -1,2 +1,16 @@
+import java.util.Arrays;
+
 public class ArrayPractice {
+    public static void main(String[] args) {
+
+        int [] arr1= new int[]{2,55,6};
+        int [] arr2 =new int[]{2,55,6};
+        if(Arrays.equals(arr1,arr2)){
+            System.out.println("true");
+        }else
+        System.out.println("false");
+
+        Arrays.sort(arr1);
+        System.out.println(Arrays.toString(arr1));
+    }
 }
